8d2ab88f50dff9b4411133c286c622167c322c37
23pointsNorth@gmail.com <23pointsNorth@gmail.com@21b84622-9f59-52c5-3126-5b6bce64854a>
2013-02-28 22:05:10 +0000

    Added a tool for measuring positions.

--- trunk/Matlab/MapRiverBed.m
+++ trunk/Matlab/MapRiverBed.m
@@ -1,12 +1,33 @@
 function MapRiverBed()
 
-%file = strcat('N:\\..University\\Year2\\Cybs Challenge\\Data', filename);
+%file = strcat('N:\\Data\\', filename);
 
-left = load('N:\..University\Year2\Cybs Challenge\Data\RiverDataLeft.txt');
-right = load('N:\..University\Year2\Cybs Challenge\Data\RiverDataRight.txt');
-dist = load('N:\..University\Year2\Cybs Challenge\Data\DistData.txt');
+left = load('N:\\Data\\RiverDataLeft.txt');
+right = load('N:\\Data\\RiverDataRight.txt');
+dist = load('N:\\Data\\DistData.txt');
 
 figure;
 plot(left, dist, '.' ,right, dist, '.');
+
+xlabel('Distance in (mm)');
+ylabel('Deflection from Rover (mm)');
+title('River Bed Map Profile');
+grid on;
+
+%Draw line tool 1
+peakX = [600 500];
+peakY = [300 200];
+l1 = imdistline(gca, peakX, peakY);
+api1 = iptgetapi(l1);
+api1.setLabelTextFormatter('%02.0f mm');
+api1.setColor('r');
+
+%Draw line tool 2
+peakX = [200 500];
+peakY = [100 250];
+l2 = imdistline(gca, peakX, peakY);
+api2 = iptgetapi(l2);
+api2.setLabelTextFormatter('%02.0f mm');
+api2.setColor('g');
 
 end
