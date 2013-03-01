cf08ac9f3be764d350295b6adbe9c7a659906dd8
23pointsNorth@gmail.com <23pointsNorth@gmail.com@21b84622-9f59-52c5-3126-5b6bce64854a>
2013-03-01 09:51:51 +0000

    Added averaging filter on the Matlab code.

--- Matlab/MapRiverBed.m
+++ Matlab/MapRiverBed.m
@@ -1,10 +1,24 @@
 function MapRiverBed()
 
+IR_SCALE_FACTOR = 1;
+DISTANCE_SCALE_FACTOR = 0.95;
+windowSize = 6;
+
 %file = strcat('N:\\Data\\', filename);
 
 left = load('N:\..University\Year2\Cybs Challenge\Data\\RiverDataLeft.txt');
 right = load('N:\..University\Year2\Cybs Challenge\Data\\RiverDataRight.txt');
 dist = load('N:\..University\Year2\Cybs Challenge\Data\\DistData.txt');
+
+
+left = filter(ones(1,windowSize)/windowSize, 1, left);
+right = filter(ones(1,windowSize)/windowSize, 1, right);
+
+left = left * IR_SCALE_FACTOR;
+right = right * IR_SCALE_FACTOR;
+dist = dist * DISTANCE_SCALE_FACTOR;
+
+
 
 figure;
 plot(dist, left, 'r' ,dist, right,'m');
@@ -15,8 +29,8 @@ title('River Bed Map Profile');
 grid on;
 
 %Draw line tool 1
-peakX = [600 500];
-peakY = [300 200];
+peakX = [450 550];
+peakY = [180 150];
 l1 = imdistline(gca, peakX, peakY);
 api1 = iptgetapi(l1);
 api1.setLabelTextFormatter('%02.0f mm');
@@ -24,7 +38,7 @@ api1.setColor('r');
 
 %Draw line tool 2
 peakX = [200 500];
-peakY = [100 250];
+peakY = [175 150];
 l2 = imdistline(gca, peakX, peakY);
 api2 = iptgetapi(l2);
 api2.setLabelTextFormatter('%02.0f mm');
