1fb6dcb20eeba28195ebcb51ff24b9010ff09736
23pointsNorth@gmail.com <23pointsNorth@gmail.com@21b84622-9f59-52c5-3126-5b6bce64854a>
2013-03-12 15:16:41 +0000

    Edited crashed rocket. Now receiving simple decoding!

--- trunk/GUI/WIFIGUIDemo/WIFIGUIDemo/CrashedRocketForm.cs
+++ trunk/GUI/WIFIGUIDemo/WIFIGUIDemo/CrashedRocketForm.cs
@@ -17,20 +17,75 @@ namespace WIFIGUIDemo
 
         public CrashedRocketForm(Main mForm, TCPClient theClient)
         {
-            InitializeComponent();
             mainForm = mForm;
             rover = theClient;
+            InitializeComponent();
+
+            rover.OnMessageReceived += new ClientBase.ClientMessageReceivedEvent(DataReceivedCrashed_Handler);
         }
 
         private void sendEmailButton_Click(object sender, EventArgs e)
         {
             System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
-            message.To.Add("kp015255@reading.ac.uk  ");
-            message.Subject = "Hello message!";
+            message.To.Add("kp015255@reading.ac.uk");
+            message.Subject = "Crashed Rocket Message!";
             message.From = new System.Net.Mail.MailAddress("kp015255@reading.ac.uk");
-            message.Body = MessageBox.Text;
+            message.Body = "Hello Dan, \n\t We are happy to announce that we have reached the crashed rocket! " +
+                "We recevied and decoded the following message: \n\n \t\t \"" + decodedMessage.Text + "\""+
+                " \n\n \t Hope this helps the mission!" + "\n\n Best wishes,\n \t Project Nova\n \t Cyb01";
             System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("smtp.reading.ac.uk");
             smtp.Send(message);
         }
+
+        private void requestMessageButton_Click(object sender, EventArgs e)
+        {
+            if (rover.isConnected)
+            {
+                rover.SendData(CommandID.GetCrashedRoverData, new byte[] { });
+            }
+            else
+            {
+                MessageBox.Show("Not connected!");
+            }
+        }
+
+
+        private void DataReceivedCrashed_Handler(Client_Message_EventArgs e)
+        {
+            List<byte> NewData = e.RawMessage.ToList();
+
+            if (NewData.Count > 3)
+            {
+                switch (NewData[3])
+                {
+                    case (byte)CommandID.GetCrashedRoverData:
+                        {
+                            //Invokation to allow cross thread manipulation
+                            this.BeginInvoke(new EventHandler(delegate
+                            {
+                                for (int i = 4; i < NewData.Count; i++)
+                                {
+                                    CrashedMessageBox.Text += NewData[i] + " ";
+                                    if (i % 2 == 0) decodedMessage.Text += ((char)(NewData[i] / 2));
+                                }
+                            }));
+                            break;
+                        }   
+                }
+            }
+        }
+
+        private void CrashedRocketForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Unsubscribe
+            rover.OnMessageReceived -= new ClientBase.ClientMessageReceivedEvent(DataReceivedCrashed_Handler);
+        }
+
+        private void clearMsgButton_Click(object sender, EventArgs e)
+        {
+            CrashedMessageBox.Clear();
+            decodedMessage.Clear();
+        }
+
     }
 }
