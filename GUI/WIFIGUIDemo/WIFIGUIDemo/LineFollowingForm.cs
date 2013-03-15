0307a43a7313a67f0e2c1d29e4bbf90933547038
roomanson@gmail.com <roomanson@gmail.com@21b84622-9f59-52c5-3126-5b6bce64854a>
2013-03-15 17:48:41 +0000

    line following begins anew. calibration set up and basic motor control

--- GUI/WIFIGUIDemo/WIFIGUIDemo/LineFollowingForm.cs
+++ GUI/WIFIGUIDemo/WIFIGUIDemo/LineFollowingForm.cs
@@ -12,6 +12,15 @@ namespace WIFIGUIDemo
 {
     public partial class LineFollowingForm : Form
     {
+        int max = 0, min = 500;
+        int angle = 5;
+        int angle_turned = 0;
+        bool calibrate = false;
+        bool turn_left = false;
+        public bool lineFollowing = false;
+        int maxspeed = 127, minspeed = 50;
+
+        
         Main mainForm;
         TCPClient rover;
 
@@ -46,6 +55,57 @@ namespace WIFIGUIDemo
 
                             leftValueLabel.Text = left_value.ToString();
                             rightValueLabel.Text = right_value.ToString();
+
+                            float ls = 0;
+                            float rs = 0;
+
+                            if (lineFollowing)
+                            {
+                                ls = minspeed + ((maxspeed - minspeed) * (left_value / max));
+                                rs = minspeed + ((maxspeed - minspeed) * (right_value / max));
+
+                                rover.SendData(CommandID.SetMotorsSpeed, new byte[] { (byte)ls, (byte)(rs) });
+                                rover.SendData(CommandID.IRDistanceData, new byte[] { });
+                            }
+
+                            if (calibrate)
+                            {
+                                if (left_value > max)
+                                    max = left_value;
+                                if (right_value > max)
+                                    max = right_value;
+                                if (left_value < min)
+                                    min = left_value;
+                                if (right_value < min)
+                                    min = right_value;
+                                max_Label.Text = max.ToString();
+                                min_Label.Text = min.ToString();
+
+                                if ((angle_turned < 45) && (turn_left==false))
+                                {
+                                    angle_turned = angle_turned + 5;
+                                    turn_Calibrate(angle);
+                                    if (angle_turned == 0)
+                                        calibrate = false;
+                                }
+                                else
+                                {
+                                    turn_left = true;
+                                    angle = -5;
+                                }
+
+                                if ((turn_left == true) && (angle_turned > -45))
+                                {
+                                    angle_turned = angle_turned - 5;
+                                    turn_Calibrate(angle);
+                                }
+                                else
+                                {
+                                    angle = +5;
+                                    turn_left = false;
+                                }
+
+                            }
                         }));
                         break;
                 }
@@ -54,12 +114,73 @@ namespace WIFIGUIDemo
 
         private void calibrateButton_Click(object sender, EventArgs e)
         {
+            max = 0;
+            min = 500;
+            calibrate = true;
+            turn_left = false;
+            angle_turned = 5;
+            turn_Calibrate(angle);
+/*           for (int i = 0; i < 9; i++)
+            {
+                finished = false;
+                turn_Degrees(angle);
+
+                rover.SendData(CommandID.IRDistanceData, new byte[] { });
+            }
+            angle = -5;
+            for (int i = 0; i < 18; i++)
+            {
+                finished = false;
+                turn_Degrees(angle);
+                rover.SendData(CommandID.IRDistanceData, new byte[] { });
+
+                while (!finished) { }
+            }
+            angle = 5;
+            
+            for (int i = 0; i < 9; i++)
+            {
+                finished = false;
+                turn_Degrees(angle);
+                rover.SendData(CommandID.IRDistanceData, new byte[] { });
+
+                while (!finished) { }
+            }*/
+        }
+
+        private void turn_Calibrate(int angle)
+        {
+           turnTimer.Interval = (int)(Math.Abs(angle) * Main._360_TURN_COEF) / 360;
+           turnTimer.Enabled = true;
+           waitTimer.Enabled = true;
+
+           int ls = Main.TURN_SPEED * Math.Sign(angle);
+           int rs = -ls;
+
+           rover.SendData(CommandID.SetMotorsSpeed, new byte[] { (byte)ls, (byte)(rs) });
+        }
+
+        private void waitTimer_Tick(object sender, EventArgs e)
+        {
             rover.SendData(CommandID.IRDistanceData, new byte[] { });
+            turnTimer.Enabled = false;
+        }
+
+        private void turnTimer_Tick(object sender, EventArgs e)
+        {
+            rover.SendData(CommandID.SetMotorsSpeed, new byte[] { 0, 0 });
+            turnTimer.Enabled = false;
         }
 
         private void LineFollowingForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             rover.OnMessageReceived -= new ClientBase.ClientMessageReceivedEvent(DataReceivedLine_Handler);
+        }
+
+        private void followLineButton_Click(object sender, EventArgs e)
+        {
+            lineFollowing = !lineFollowing;
+            rover.SendData(CommandID.IRDistanceData, new byte[] { });
         }
     }
 }
