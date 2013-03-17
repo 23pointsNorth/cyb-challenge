8dfb3e0f31819d45aa0c7146968913fe14e28796
23pointsNorth@gmail.com <23pointsNorth@gmail.com@21b84622-9f59-52c5-3126-5b6bce64854a>
2013-03-17 18:32:48 +0000

    Updated line following. Not Done.

--- GUI/WIFIGUIDemo/WIFIGUIDemo/LineFollowingForm.Designer.cs
+++ GUI/WIFIGUIDemo/WIFIGUIDemo/LineFollowingForm.Designer.cs
@@ -35,15 +35,17 @@
             this.rightValueLabel = new System.Windows.Forms.Label();
             this.calibrationTimer = new System.Windows.Forms.Timer(this.components);
             this.dataTimer = new System.Windows.Forms.Timer(this.components);
-            this.max_Label = new System.Windows.Forms.Label();
-            this.min_Label = new System.Windows.Forms.Label();
             this.turnTimer = new System.Windows.Forms.Timer(this.components);
             this.waitTimer = new System.Windows.Forms.Timer(this.components);
+            this.calibrationStatusLabel = new System.Windows.Forms.Label();
+            this.maxCalibTextBox = new System.Windows.Forms.TextBox();
+            this.minCalibTextBox = new System.Windows.Forms.TextBox();
+            this.onLineStatusLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // followLineButton
             // 
-            this.followLineButton.Location = new System.Drawing.Point(32, 17);
+            this.followLineButton.Location = new System.Drawing.Point(12, 12);
             this.followLineButton.Name = "followLineButton";
             this.followLineButton.Size = new System.Drawing.Size(75, 23);
             this.followLineButton.TabIndex = 0;
@@ -53,7 +55,7 @@
             // 
             // calibrateButton
             // 
-            this.calibrateButton.Location = new System.Drawing.Point(32, 46);
+            this.calibrateButton.Location = new System.Drawing.Point(12, 41);
             this.calibrateButton.Name = "calibrateButton";
             this.calibrateButton.Size = new System.Drawing.Size(75, 23);
             this.calibrateButton.TabIndex = 1;
@@ -64,7 +66,7 @@
             // leftValueLabel
             // 
             this.leftValueLabel.AutoSize = true;
-            this.leftValueLabel.Location = new System.Drawing.Point(124, 25);
+            this.leftValueLabel.Location = new System.Drawing.Point(93, 12);
             this.leftValueLabel.Name = "leftValueLabel";
             this.leftValueLabel.Size = new System.Drawing.Size(55, 13);
             this.leftValueLabel.TabIndex = 2;
@@ -73,7 +75,7 @@
             // rightValueLabel
             // 
             this.rightValueLabel.AutoSize = true;
-            this.rightValueLabel.Location = new System.Drawing.Point(185, 25);
+            this.rightValueLabel.Location = new System.Drawing.Point(154, 12);
             this.rightValueLabel.Name = "rightValueLabel";
             this.rightValueLabel.Size = new System.Drawing.Size(62, 13);
             this.rightValueLabel.TabIndex = 3;
@@ -87,24 +89,6 @@
             // 
             this.dataTimer.Interval = 75;
             // 
-            // max_Label
-            // 
-            this.max_Label.AutoSize = true;
-            this.max_Label.Location = new System.Drawing.Point(32, 76);
-            this.max_Label.Name = "max_Label";
-            this.max_Label.Size = new System.Drawing.Size(27, 13);
-            this.max_Label.TabIndex = 4;
-            this.max_Label.Text = "Max";
-            // 
-            // min_Label
-            // 
-            this.min_Label.AutoSize = true;
-            this.min_Label.Location = new System.Drawing.Point(72, 76);
-            this.min_Label.Name = "min_Label";
-            this.min_Label.Size = new System.Drawing.Size(24, 13);
-            this.min_Label.TabIndex = 5;
-            this.min_Label.Text = "Min";
-            // 
             // turnTimer
             // 
             this.turnTimer.Interval = 1000;
@@ -115,13 +99,51 @@
             this.waitTimer.Interval = 1000;
             this.waitTimer.Tick += new System.EventHandler(this.waitTimer_Tick);
             // 
+            // calibrationStatusLabel
+            // 
+            this.calibrationStatusLabel.AutoSize = true;
+            this.calibrationStatusLabel.Location = new System.Drawing.Point(9, 67);
+            this.calibrationStatusLabel.Name = "calibrationStatusLabel";
+            this.calibrationStatusLabel.Size = new System.Drawing.Size(73, 13);
+            this.calibrationStatusLabel.TabIndex = 6;
+            this.calibrationStatusLabel.Text = "Calibration Off";
+            // 
+            // maxCalibTextBox
+            // 
+            this.maxCalibTextBox.Location = new System.Drawing.Point(12, 83);
+            this.maxCalibTextBox.Name = "maxCalibTextBox";
+            this.maxCalibTextBox.Size = new System.Drawing.Size(28, 20);
+            this.maxCalibTextBox.TabIndex = 7;
+            this.maxCalibTextBox.Text = "520";
+            this.maxCalibTextBox.TextChanged += new System.EventHandler(this.maxCalibTextBox_TextChanged);
+            // 
+            // minCalibTextBox
+            // 
+            this.minCalibTextBox.Location = new System.Drawing.Point(46, 83);
+            this.minCalibTextBox.Name = "minCalibTextBox";
+            this.minCalibTextBox.Size = new System.Drawing.Size(28, 20);
+            this.minCalibTextBox.TabIndex = 8;
+            this.minCalibTextBox.Text = "10";
+            this.minCalibTextBox.TextChanged += new System.EventHandler(this.minCalibTextBox_TextChanged);
+            // 
+            // onLineStatusLabel
+            // 
+            this.onLineStatusLabel.AutoSize = true;
+            this.onLineStatusLabel.Location = new System.Drawing.Point(12, 106);
+            this.onLineStatusLabel.Name = "onLineStatusLabel";
+            this.onLineStatusLabel.Size = new System.Drawing.Size(42, 13);
+            this.onLineStatusLabel.TabIndex = 9;
+            this.onLineStatusLabel.Text = "IR data";
+            // 
             // LineFollowingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(429, 311);
-            this.Controls.Add(this.min_Label);
-            this.Controls.Add(this.max_Label);
+            this.Controls.Add(this.onLineStatusLabel);
+            this.Controls.Add(this.minCalibTextBox);
+            this.Controls.Add(this.maxCalibTextBox);
+            this.Controls.Add(this.calibrationStatusLabel);
             this.Controls.Add(this.rightValueLabel);
             this.Controls.Add(this.leftValueLabel);
             this.Controls.Add(this.calibrateButton);
@@ -142,9 +164,11 @@
         private System.Windows.Forms.Label rightValueLabel;
         private System.Windows.Forms.Timer calibrationTimer;
         private System.Windows.Forms.Timer dataTimer;
-        private System.Windows.Forms.Label max_Label;
-        private System.Windows.Forms.Label min_Label;
         private System.Windows.Forms.Timer turnTimer;
         private System.Windows.Forms.Timer waitTimer;
+        private System.Windows.Forms.Label calibrationStatusLabel;
+        private System.Windows.Forms.TextBox maxCalibTextBox;
+        private System.Windows.Forms.TextBox minCalibTextBox;
+        private System.Windows.Forms.Label onLineStatusLabel;
     }
 }
